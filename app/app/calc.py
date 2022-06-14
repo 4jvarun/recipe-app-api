9886860479e2786b7b38524a67f6d1700e42d6c8
pknowledge <4jvarun@gmail.com>
2022-06-14 23:33:34 +0530

    Added dependencies

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return y-x
+
+# """
+# Calculator functions
+# """
+
+
+# def add(x, y):
+#     """Add x and y and return result."""
+#     return x + y
